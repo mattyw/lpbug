68054ef1815aa0a215591005a35a07d3f9a45725
mattyw <mattyw@me.com>
2014-01-13 17:58:01 +0000

    usage

---
+++ lpbug.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
+)
+
+// run go install to but this into your path.
+// Open this file (or any other in vim)
+// Find bug links like this lp:1
+// get to the start of the word then
+// :!lpbug C-rC-a
+func main() {
+	args := os.Args
+	if len(args) != 2 {
+		fmt.Println("Take a single argument in the form of the bug number")
+		fmt.Println("Uses regular expressions to find the number so lp: and bug: are accepted")
+		return
+	}
+
+	bug := args[1]
+	re := regexp.MustCompile("[0-9]+")
+	number := re.FindString(bug)
+	url := fmt.Sprintf("http://launchpad.net/bugs/%s", number)
+
+	cmd := exec.Command("sensible-browser", url)
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
